7608753fd742290d96ce926367724d36707ba55c
Giovanni Trovato <giovanni.trovato@cortado.com>
2022-10-10 18:30:35 +0200

    Initial Commit

---
+++ Bonjur Printer Generator/Bonjur_Printer_GeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bonjur_Printer_GeneratorApp.swift
+//  Bonjur Printer Generator
+//
+//  Created by Giovanni Trovato on 10/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Bonjur_Printer_GeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
